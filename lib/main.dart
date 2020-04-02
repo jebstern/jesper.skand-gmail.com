e44eeec4a888b88e3785080c3a92a0245a9351e5
Jesper Skand <jesper.skand@gmail.com>
2020-04-02 11:17:05 +0300

    Lots of refactoring and improvements

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/tictactoe.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tic tac toe',
+      title: "Tic Tac Toe",
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -24,362 +24,99 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> tiles = ["", "", "", "", "", "", "", "", ""];
-  String turn = "X";
-  int playerWins = 0;
-  int computerWins = 0;
-  int draws = 0;
-  Random random = new Random();
-  AppBar appBar = AppBar(
-    title: Text("Tic Tac Toe"),
-  );
-  double viewHeight = 0;
+  List<String> names = ["Alex", "Bellamy", "Charlie", "Dakota"];
+  String dropdownValue = "Alex";
 
   @override
   Widget build(BuildContext context) {
-    executeAfterBuild();
     return Scaffold(
-      appBar: appBar,
-      body: _getBody(context),
-    );
-  }
-
-  Widget _getBody(BuildContext context) {
-    double statusBarHeight = MediaQuery.of(context).padding.top;
-    double appBarHeight = appBar.preferredSize.height;
-    viewHeight =
-        MediaQuery.of(context).size.height - statusBarHeight - appBarHeight;
-
-    return Column(
-      children: <Widget>[
-        Container(
-          height: viewHeight * 0.75,
-          child: Column(
-            children: _getTiles(),
-          ),
+      appBar: AppBar(
+        title: Text(
+          "Sign in",
         ),
-        Container(
-          height: viewHeight * 0.25,
-          color: Colors.redAccent,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: <Widget>[
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      "Turn: " + (turn == "X" ? "Player" : "Computer"),
-                      style: TextStyle(
-                        fontSize: 24,
-                      ),
-                    ),
-                    Text(
-                      "Player wins: " + playerWins.toString(),
-                    ),
-                    Text(
-                      "Computer wins: " + computerWins.toString(),
-                    ),
-                    Text(
-                      "Draws: " + draws.toString(),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Future<void> executeAfterBuild() async {
-    if (turn == "O") {
-      Future.delayed(const Duration(milliseconds: 1000), () {
-        int randomNumber = _getBestTileSelection();
-        _setTileValue(randomNumber);
-      });
-    }
-  }
-
-  List<Widget> _getTiles() {
-    return [
-      Expanded(
-        child: Row(
-          children: <Widget>[
-            _getTile(0),
-            _getTile(1),
-            _getTile(2),
-          ],
-        ),
+        automaticallyImplyLeading: false,
       ),
-      Expanded(
-        child: Row(
-          children: <Widget>[
-            _getTile(3),
-            _getTile(4),
-            _getTile(5),
-          ],
-        ),
-      ),
-      Expanded(
-        child: Row(
-          children: <Widget>[
-            _getTile(6),
-            _getTile(7),
-            _getTile(8),
-          ],
-        ),
-      ),
-    ];
-  }
-
-  Widget _getTile(int index) {
-    return Expanded(
-      child: GestureDetector(
-        onTap: () {
-          setState(() {
-            _setTileValue(index);
-          });
-        },
+      body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(16.0),
-            child: Container(
-              color: tiles[index] == ""
-                  ? Colors.blueAccent
-                  : (tiles[index] == "X"
-                      ? Colors.redAccent
-                      : Colors.greenAccent),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    tiles[index],
-                    style: TextStyle(
-                      fontSize: (viewHeight * 0.75 / 3 - 48),
-                    ),
-                  ),
-                ],
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                "Account sign in",
+                style: TextStyle(fontSize: 32),
               ),
-            ),
+              SizedBox(
+                height: 12.0,
+              ),
+              Text(
+                "Everything is simplified for demo purposes",
+                style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                  fontSize: 12,
+                ),
+              ),
+              SizedBox(
+                height: 12.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: DropdownButton<String>(
+                  value: dropdownValue,
+                  icon: Icon(Icons.account_circle),
+                  iconSize: 24,
+                  isExpanded: true,
+                  elevation: 16,
+                  style: TextStyle(color: Colors.deepPurple),
+                  underline: Container(
+                    height: 2,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  onChanged: (String newValue) {
+                    setState(() {
+                      dropdownValue = newValue;
+                    });
+                  },
+                  items: names.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                  hint: Text("data"),
+                ),
+              ),
+              SizedBox(
+                height: 42.0,
+              ),
+              RaisedButton(
+                onPressed: () {
+                  _onLogin();
+                },
+                child: Text(
+                  "Let's play",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                color: Theme.of(context).primaryColor,
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 
-  void _setTileValue(int index) {
-    print("_setTileValue: " + index.toString());
-    if (tiles[index] == "") {
-      tiles[index] = turn;
-      bool gameOver = _isGameOver();
-      List<String> tilesCopy = List.from(tiles);
-      tilesCopy.removeWhere((val) => val == "");
-      print("Game over:" + gameOver.toString());
-      if (gameOver) {
-        _showGameOverDialog(turn == "X" ? "Player won!" : "Computer won!");
-        _addWin();
-      } else if (tilesCopy.length == 9) {
-        _showGameOverDialog("It's a draw! How exciting...");
-        _addDraw();
-      } else {
-        setState(() {
-          turn = turn == "X" ? "O" : "X";
-        });
-      }
-    }
-  }
-
-  bool _isGameOver() {
-    // Horizontal
-    if (tiles[0] == tiles[1] && tiles[0] == tiles[2] && tiles[0] != "") {
-      return true;
-    }
-    if (tiles[3] == tiles[4] && tiles[3] == tiles[5] && tiles[3] != "") {
-      return true;
-    }
-    if (tiles[6] == tiles[7] && tiles[6] == tiles[8] && tiles[6] != "") {
-      return true;
-    }
-
-    // Vertical
-    if (tiles[0] == tiles[3] && tiles[0] == tiles[6] && tiles[0] != "") {
-      return true;
-    }
-    if (tiles[1] == tiles[4] && tiles[1] == tiles[7] && tiles[1] != "") {
-      return true;
-    }
-    if (tiles[2] == tiles[5] && tiles[2] == tiles[8] && tiles[2] != "") {
-      return true;
-    }
-
-    // Diagonal
-    if (tiles[0] == tiles[4] && tiles[0] == tiles[8] && tiles[0] != "") {
-      return true;
-    }
-    if (tiles[2] == tiles[4] && tiles[2] == tiles[6] && tiles[2] != "") {
-      return true;
-    }
-
-    return false;
-  }
-
-  int _getBestTileSelection() {
-    // Winning moves / Win game
-    if (tiles[0] == tiles[3] && tiles[0] == "O" && tiles[6] == "") {
-      return 6;
-    }
-    if (tiles[0] == tiles[4] && tiles[0] == "O" && tiles[8] == "") {
-      return 8;
-    }
-    if (tiles[0] == tiles[1] && tiles[0] == "O" && tiles[2] == "") {
-      return 2;
-    }
-    if (tiles[1] == tiles[4] && tiles[1] == "O" && tiles[7] == "") {
-      return 7;
-    }
-    if (tiles[1] == tiles[2] && tiles[1] == "O" && tiles[0] == "") {
-      return 0;
-    }
-    if (tiles[2] == tiles[4] && tiles[2] == "O" && tiles[6] == "") {
-      return 6;
-    }
-    if (tiles[2] == tiles[5] && tiles[2] == "O" && tiles[8] == "") {
-      return 8;
-    }
-    if (tiles[3] == tiles[4] && tiles[3] == "O" && tiles[5] == "") {
-      return 5;
-    }
-    if (tiles[3] == tiles[6] && tiles[3] == "O" && tiles[0] == "") {
-      return 0;
-    }
-    if (tiles[4] == tiles[5] && tiles[4] == "O" && tiles[3] == "") {
-      return 3;
-    }
-    if (tiles[4] == tiles[6] && tiles[4] == "O" && tiles[2] == "") {
-      return 2;
-    }
-    if (tiles[4] == tiles[7] && tiles[4] == "O" && tiles[1] == "") {
-      return 1;
-    }
-    if (tiles[5] == tiles[8] && tiles[5] == "O" && tiles[2] == "") {
-      return 2;
-    }
-    if (tiles[6] == tiles[7] && tiles[6] == "O" && tiles[8] == "") {
-      return 8;
-    }
-    if (tiles[7] == tiles[8] && tiles[7] == "O" && tiles[6] == "") {
-      return 6;
-    }
-    if (tiles[8] == tiles[4] && tiles[8] == "O" && tiles[0] == "") {
-      return 0;
-    }
-
-    // Blocking moves / Don't let player win
-    if (tiles[0] == tiles[3] && tiles[0] == "X" && tiles[6] == "") {
-      return 6;
-    }
-    if (tiles[0] == tiles[4] && tiles[0] == "X" && tiles[8] == "") {
-      return 8;
-    }
-    if (tiles[0] == tiles[1] && tiles[0] == "X" && tiles[2] == "") {
-      return 2;
-    }
-    if (tiles[1] == tiles[4] && tiles[1] == "X" && tiles[7] == "") {
-      return 7;
-    }
-    if (tiles[1] == tiles[2] && tiles[1] == "X" && tiles[0] == "") {
-      return 0;
-    }
-    if (tiles[2] == tiles[4] && tiles[2] == "X" && tiles[6] == "") {
-      return 6;
-    }
-    if (tiles[2] == tiles[5] && tiles[2] == "X" && tiles[8] == "") {
-      return 8;
-    }
-    if (tiles[3] == tiles[4] && tiles[3] == "X" && tiles[5] == "") {
-      return 5;
-    }
-    if (tiles[3] == tiles[6] && tiles[3] == "X" && tiles[0] == "") {
-      return 0;
-    }
-    if (tiles[4] == tiles[5] && tiles[4] == "X" && tiles[3] == "") {
-      return 3;
-    }
-    if (tiles[4] == tiles[6] && tiles[4] == "X" && tiles[2] == "") {
-      return 2;
-    }
-    if (tiles[4] == tiles[7] && tiles[4] == "X" && tiles[1] == "") {
-      return 1;
-    }
-    if (tiles[5] == tiles[8] && tiles[5] == "X" && tiles[2] == "") {
-      return 2;
-    }
-    if (tiles[6] == tiles[7] && tiles[6] == "X" && tiles[8] == "") {
-      return 8;
-    }
-    if (tiles[7] == tiles[8] && tiles[7] == "X" && tiles[6] == "") {
-      return 6;
-    }
-    if (tiles[8] == tiles[4] && tiles[8] == "X" && tiles[0] == "") {
-      return 0;
-    }
-    int randomNumber = random.nextInt(8);
-    while (tiles[randomNumber] != "") {
-      randomNumber = random.nextInt(8);
-    }
-    return randomNumber;
-  }
-
-  Future<void> _showGameOverDialog(String text) async {
-    return showDialog<void>(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Game over'),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                Text(
-                  text,
-                ),
-              ],
-            ),
-          ),
-          actions: <Widget>[
-            FlatButton(
-              child: Text('OK'),
-              onPressed: () {
-                setState(() {
-                  tiles = ["", "", "", "", "", "", "", "", ""];
-                });
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
+  void _onLogin() async {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => TicTacToePage(player: dropdownValue,)),
     );
-  }
-
-  void _addWin() {
-    setState(() {
-      if (turn == "X") {
-        playerWins++;
-      } else {
-        computerWins++;
-      }
-      turn = "X";
-    });
-  }
-
-  void _addDraw() {
-    setState(() {
-      draws++;
-      turn = "X";
-    });
   }
 }
---
+++ lib/tictactoe.dart
@@ -0,0 +1,474 @@
+import 'dart:convert';
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/main.dart';
+import 'package:http/http.dart' as http;
+
+class TicTacToePage extends StatefulWidget {
+  final String player;
+
+  TicTacToePage({Key key, @required this.player}) : super(key: key);
+
+  @override
+  _TicTacToePageState createState() => _TicTacToePageState();
+}
+
+class _TicTacToePageState extends State<TicTacToePage> {
+  List<String> tiles = ["", "", "", "", "", "", "", "", ""];
+  String turn = "X";
+  int playerWins = 0;
+  int computerWins = 0;
+  int draws = 0;
+  Random random = new Random();
+  AppBar appBar;
+  double viewHeight = 0;
+  bool moveAllowed = true;
+  List<String> appbarActions = ["Log out", "Upload"];
+
+  @override
+  Widget build(BuildContext context) {
+    appBar = AppBar(
+      title: Text("Tic Tac Toe"),
+      actions: <Widget>[
+        PopupMenuButton<String>(
+          onSelected: (val) {
+            if (val.toLowerCase() == "upload") {
+              _showDialog();
+              _uploadResults();
+            } else {
+              _logout();
+            }
+          },
+          itemBuilder: (BuildContext context) {
+            return appbarActions.map((String choice) {
+              return PopupMenuItem<String>(
+                value: choice,
+                child: Text(choice),
+              );
+            }).toList();
+          },
+        )
+      ],
+      automaticallyImplyLeading: false,
+    );
+
+    executeAfterBuild();
+    return Scaffold(
+      appBar: appBar,
+      body: _getBody(context),
+    );
+  }
+
+  Widget _getBody(BuildContext context) {
+    double statusBarHeight = MediaQuery.of(context).padding.top;
+    double appBarHeight = appBar.preferredSize.height;
+    viewHeight =
+        MediaQuery.of(context).size.height - statusBarHeight - appBarHeight;
+
+    return Column(
+      children: <Widget>[
+        Container(
+          height: viewHeight * 0.75,
+          child: Column(
+            children: _getTiles(),
+          ),
+        ),
+        Container(
+          height: viewHeight * 0.25,
+          color: Colors.redAccent,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Turn: " + (turn == "X" ? widget.player : "Computer"),
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                    Text(
+                      "Player wins: " + playerWins.toString(),
+                    ),
+                    Text(
+                      "Computer wins: " + computerWins.toString(),
+                    ),
+                    Text(
+                      "Draws: " + draws.toString(),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Future<void> executeAfterBuild() async {
+    if (turn == "O") {
+      Future.delayed(const Duration(seconds: 1), () {
+        int randomNumber = _getBestTileSelection();
+        _setTileValue(randomNumber);
+      });
+    }
+  }
+
+  List<Widget> _getTiles() {
+    return [
+      Expanded(
+        child: Row(
+          children: <Widget>[
+            _getTile(0),
+            _getTile(1),
+            _getTile(2),
+          ],
+        ),
+      ),
+      Expanded(
+        child: Row(
+          children: <Widget>[
+            _getTile(3),
+            _getTile(4),
+            _getTile(5),
+          ],
+        ),
+      ),
+      Expanded(
+        child: Row(
+          children: <Widget>[
+            _getTile(6),
+            _getTile(7),
+            _getTile(8),
+          ],
+        ),
+      ),
+    ];
+  }
+
+  Widget _getTile(int index) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () {
+          if (moveAllowed) {
+            setState(() {
+              _setTileValue(index);
+            });
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(16.0),
+            child: Container(
+              color: tiles[index] == ""
+                  ? Colors.blueAccent
+                  : (tiles[index] == "X"
+                      ? Colors.redAccent
+                      : Colors.greenAccent),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    tiles[index],
+                    style: TextStyle(
+                      fontSize: (viewHeight * 0.75 / 3 - 48),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _setTileValue(int index) {
+    if (tiles[index] == "") {
+      tiles[index] = turn;
+      bool gameOver = _isGameOver();
+      List<String> tilesCopy = List.from(tiles);
+      tilesCopy.removeWhere((val) => val == "");
+      if (gameOver) {
+        _showGameOverDialog(turn == "X" ? "Player won!" : "Computer won!");
+        _addWin();
+      } else if (tilesCopy.length == 9) {
+        _showGameOverDialog("It's a draw! How exciting...");
+        _addDraw();
+      } else {
+        setState(() {
+          if (turn == "X") {
+            moveAllowed = false;
+            turn = "O";
+          } else {
+            turn = "X";
+            moveAllowed = true;
+          }
+        });
+      }
+    }
+  }
+
+  bool _isGameOver() {
+    // Horizontal
+    if (tiles[0] == tiles[1] && tiles[0] == tiles[2] && tiles[0] != "") {
+      return true;
+    }
+    if (tiles[3] == tiles[4] && tiles[3] == tiles[5] && tiles[3] != "") {
+      return true;
+    }
+    if (tiles[6] == tiles[7] && tiles[6] == tiles[8] && tiles[6] != "") {
+      return true;
+    }
+
+    // Vertical
+    if (tiles[0] == tiles[3] && tiles[0] == tiles[6] && tiles[0] != "") {
+      return true;
+    }
+    if (tiles[1] == tiles[4] && tiles[1] == tiles[7] && tiles[1] != "") {
+      return true;
+    }
+    if (tiles[2] == tiles[5] && tiles[2] == tiles[8] && tiles[2] != "") {
+      return true;
+    }
+
+    // Diagonal
+    if (tiles[0] == tiles[4] && tiles[0] == tiles[8] && tiles[0] != "") {
+      return true;
+    }
+    if (tiles[2] == tiles[4] && tiles[2] == tiles[6] && tiles[2] != "") {
+      return true;
+    }
+
+    return false;
+  }
+
+  int _getBestTileSelection() {
+    // Winning moves / Win game
+    if (tiles[0] == tiles[3] && tiles[0] == "O" && tiles[6] == "") {
+      return 6;
+    }
+    if (tiles[0] == tiles[4] && tiles[0] == "O" && tiles[8] == "") {
+      return 8;
+    }
+    if (tiles[0] == tiles[1] && tiles[0] == "O" && tiles[2] == "") {
+      return 2;
+    }
+    if (tiles[1] == tiles[4] && tiles[1] == "O" && tiles[7] == "") {
+      return 7;
+    }
+    if (tiles[1] == tiles[2] && tiles[1] == "O" && tiles[0] == "") {
+      return 0;
+    }
+    if (tiles[2] == tiles[4] && tiles[2] == "O" && tiles[6] == "") {
+      return 6;
+    }
+    if (tiles[2] == tiles[5] && tiles[2] == "O" && tiles[8] == "") {
+      return 8;
+    }
+    if (tiles[3] == tiles[4] && tiles[3] == "O" && tiles[5] == "") {
+      return 5;
+    }
+    if (tiles[3] == tiles[6] && tiles[3] == "O" && tiles[0] == "") {
+      return 0;
+    }
+    if (tiles[4] == tiles[5] && tiles[4] == "O" && tiles[3] == "") {
+      return 3;
+    }
+    if (tiles[4] == tiles[6] && tiles[4] == "O" && tiles[2] == "") {
+      return 2;
+    }
+    if (tiles[4] == tiles[7] && tiles[4] == "O" && tiles[1] == "") {
+      return 1;
+    }
+    if (tiles[5] == tiles[8] && tiles[5] == "O" && tiles[2] == "") {
+      return 2;
+    }
+    if (tiles[6] == tiles[7] && tiles[6] == "O" && tiles[8] == "") {
+      return 8;
+    }
+    if (tiles[7] == tiles[8] && tiles[7] == "O" && tiles[6] == "") {
+      return 6;
+    }
+    if (tiles[8] == tiles[4] && tiles[8] == "O" && tiles[0] == "") {
+      return 0;
+    }
+
+    // Blocking moves / Don't let player win
+    if (tiles[0] == tiles[3] && tiles[0] == "X" && tiles[6] == "") {
+      return 6;
+    }
+    if (tiles[0] == tiles[4] && tiles[0] == "X" && tiles[8] == "") {
+      return 8;
+    }
+    if (tiles[0] == tiles[1] && tiles[0] == "X" && tiles[2] == "") {
+      return 2;
+    }
+    if (tiles[1] == tiles[4] && tiles[1] == "X" && tiles[7] == "") {
+      return 7;
+    }
+    if (tiles[1] == tiles[2] && tiles[1] == "X" && tiles[0] == "") {
+      return 0;
+    }
+    if (tiles[2] == tiles[4] && tiles[2] == "X" && tiles[6] == "") {
+      return 6;
+    }
+    if (tiles[2] == tiles[5] && tiles[2] == "X" && tiles[8] == "") {
+      return 8;
+    }
+    if (tiles[3] == tiles[4] && tiles[3] == "X" && tiles[5] == "") {
+      return 5;
+    }
+    if (tiles[3] == tiles[6] && tiles[3] == "X" && tiles[0] == "") {
+      return 0;
+    }
+    if (tiles[4] == tiles[5] && tiles[4] == "X" && tiles[3] == "") {
+      return 3;
+    }
+    if (tiles[4] == tiles[6] && tiles[4] == "X" && tiles[2] == "") {
+      return 2;
+    }
+    if (tiles[4] == tiles[7] && tiles[4] == "X" && tiles[1] == "") {
+      return 1;
+    }
+    if (tiles[5] == tiles[8] && tiles[5] == "X" && tiles[2] == "") {
+      return 2;
+    }
+    if (tiles[6] == tiles[7] && tiles[6] == "X" && tiles[8] == "") {
+      return 8;
+    }
+    if (tiles[7] == tiles[8] && tiles[7] == "X" && tiles[6] == "") {
+      return 6;
+    }
+    if (tiles[8] == tiles[4] && tiles[8] == "X" && tiles[0] == "") {
+      return 0;
+    }
+    int randomNumber = random.nextInt(8);
+    while (tiles[randomNumber] != "") {
+      randomNumber = random.nextInt(8);
+    }
+    return randomNumber;
+  }
+
+  Future<void> _showGameOverDialog(String text) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Game over'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(
+                  text,
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('OK'),
+              onPressed: () {
+                setState(() {
+                  tiles = ["", "", "", "", "", "", "", "", ""];
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _addWin() {
+    setState(() {
+      if (turn == "X") {
+        playerWins++;
+      } else {
+        computerWins++;
+      }
+      turn = "X";
+      moveAllowed = true;
+    });
+  }
+
+  void _addDraw() {
+    setState(() {
+      draws++;
+      turn = "X";
+      moveAllowed = true;
+    });
+  }
+
+  Future<void> _showDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Uploading results'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text("This shouldn't take too long......."),
+                Padding(
+                  padding: const EdgeInsets.only(top: 12),
+                  child: LinearProgressIndicator(
+                    value: null,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[],
+        );
+      },
+    );
+  }
+
+  void _uploadResults() async {
+    Future.delayed(const Duration(seconds: 2), () {
+      _apiCall();
+    });
+  }
+
+  void _logout() async {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => MyHomePage()),
+    );
+  }
+
+  Future<void> _apiCall() async {
+    String json = jsonEncode(<String, dynamic>{
+      'username': widget.player,
+      'wins': playerWins,
+      'losses': computerWins,
+      'draws': draws,
+    });
+
+    final http.Response response = await http.put(
+      'http://192.168.10.38:3000/tic-tac-toe/update',
+      body: json,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+
+    Navigator.pop(context, null);
+
+    if (response.statusCode == 200) {
+      setState(() {
+        playerWins = 0;
+        computerWins = 0;
+        draws = 0;
+      });
+    } else {}
+  }
+}
